8173260300ddf53256ed47142e2d5c0945de3813
Aqsa <aqsahaider50@gmail.com>
2025-08-20 21:17:06 +0500

    ci: setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-// vite.config.ts
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/phishcode-site/', // <-- Must match your repo name
+// https://vite.dev/config/
+export default {
+   base: "/phishcode-site/",
   plugins: [react()],
-});
+}
