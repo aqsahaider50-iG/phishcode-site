a3b57a24d9b8a3431dafc08f3e66b12c299466dd
Aqsa <aqsahaider50@gmail.com>
2025-08-20 19:43:46 +0500

    chore: ensure pages workflow & base

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default {
   plugins: [react()],
   base: '/phishcode-site/',   // 👈 this must match your repo name
-})
+}
